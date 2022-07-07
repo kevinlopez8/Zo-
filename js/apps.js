f9e4777f49eb01000af44e0e26cca6ea5dac5eba
Kevin Lopez <kevinlopez1059@hotmail.com>
2022-07-06 21:43:35 -0300

    new-commit

---
+++ js/apps.js
@@ -0,0 +1,41 @@
+const productContainer = document.querySelector('#product-container')
+const carritoContenedor =document.querySelector('#carrito-contenedor')
+ 
+const carrito = []
+
+stockProductos.forEach((producto)=>{
+    const div = document.createElement('div')
+div.classList.add('producto')
+
+div.innerHTML = `
+        <img src=${producto.img} alt="" class="product-image">
+        <h3>${producto.nombre}</h3>
+        <p class="product-description">${producto.desc}</p>
+        <p class="productPrice">$${producto.precio}</p>
+        <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar <i class="fa-light fa-cart-shopping"></i></button>
+    `
+
+productContainer.append(div)
+})
+
+const agregarAlCarrito = (id) =>{
+    const item = stockProductos.find((prod)=> prod.id === id)
+    carrito.push(item)
+
+    renderCarrito()
+}
+
+const renderCarrito = () =>{
+
+    carrito.forEach((item)=>{
+        const div = document.querySelector('div')
+        div.classList.add('productoEnCarrito')
+
+        div.innerHTML = `
+                <p>${item.nombre}</p>
+                <p>$${item.precio}</p>
+                <button class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+            `
+        carritoContenedor.append(div) 
+    })
+}
--- js/stock.js
+++ js/stock.js
@@ -1,11 +1,7 @@
 let stockProductos = [
-    {id: 1, nombre: 'roll 1', tipo: 'Especial', desc: '', precio: 2500, img: ''},
-    {id: 2, nombre: 'roll 2', tipo: 'Especial', desc: '', precio: 2500, img: ''},
-    {id: 3, nombre: 'roll 3', tipo: 'Especial', desc: '', precio: 2500, img: ''},
-    {id: 4, nombre: 'roll 4', tipo: 'Especial', desc: '', precio: 2500, img: ''},
-    {id: 5, nombre: 'roll 5', tipo: 'Especial', desc: '', precio: 2500, img: ''},
-    {id: 6, nombre: 'roll 6', tipo: 'Especial', desc: '', precio: 2500, img: ''},
-    {id: 7, nombre: 'roll 7', tipo: 'Especial', desc: '', precio: 2500, img: ''},
-    {id: 8, nombre: 'roll 8', tipo: 'Especial', desc: '', precio: 2500, img: ''},
-    {id: 9, nombre: 'roll 9', tipo: 'Especial', desc: '', precio: 2500, img: ''},
+    {id: 1, nombre: 'Newyork Phila', tipo: 'Clasico', desc: 'Salmon, Queso y palta', precio: 1900, img: '../images/menu/newyorkphila.jpg'},
+    {id: 2, nombre: 'Newyork', tipo: 'Clasico', desc: 'Salmon y palta', precio: 1900, img: '../images/menu/newyork.jpg'},
+    {id: 3, nombre: 'Philadelphia', tipo: 'Clasico', desc: 'Salmon y Queso', precio: 1900, img: '../images/menu/philadelphia.jpg'},
+    {id: 4, nombre: 'Maki Furai', tipo: 'Especial', desc: 'Salmon, Queso y palta, rebosado en panko', precio: 1900, img: '../images/menu/makifurai.jpg'},
+    {id: 5, nombre: 'Ebi Wan', tipo: 'Entrada', desc: 'Empanadas chinas de langostinos, philadelphia y maní con salsa de jamaica', precio: 2500, img: '../images/menu/ebiwan.jpg'},
 ]
